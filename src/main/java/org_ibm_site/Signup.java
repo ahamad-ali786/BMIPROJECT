5dd2a0de40411edc64e70a8f6b4ccbc81a9c2aea
SMARTALI12 <sharmilashaik943@gmail.com>
2026-09-05 12:59:51 +0530

    Fix signup database connection

--- src/main/java/org_ibm_site/Signup.java
+++ src/main/java/org_ibm_site/Signup.java
@@ -37,22 +37,17 @@ public class Signup extends HttpServlet {
             String dbUser = System.getenv("MYSQLUSER");
             String dbPassword = System.getenv("MYSQLPASSWORD");
 
-            // Create JDBC URL
-            String url = "jdbc:mysql://" + host + ":" + port + "/" + database
-                    + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-
             System.out.println("MYSQLHOST exists: " + (host != null));
             System.out.println("MYSQLPORT exists: " + (port != null));
             System.out.println("MYSQLDATABASE exists: " + (database != null));
             System.out.println("MYSQLUSER exists: " + (dbUser != null));
             System.out.println("MYSQLPASSWORD exists: " + (dbPassword != null));
 
-            // Create database connection
-            Connection con = DriverManager.getConnection(
-                    url,
-                    dbUser,
-                    dbPassword
-            );
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database
+                    + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+            Connection con = DriverManager.getConnection(url, dbUser, dbPassword);
+            
 
             System.out.println("DATABASE CONNECTED SUCCESSFULLY");
 
